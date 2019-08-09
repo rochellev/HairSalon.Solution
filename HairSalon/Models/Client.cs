031fee4952ecdd6231d16641a31cc6e040e79416
rochellev <rochellevroberts@utexas.edu>
2019-08-09 14:33:03 -0700

    add class properties with matching database variable names

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,8 +3,8 @@ namespace HairSalon.Models
     public class Client
     {
         public int ClientId { get; set; }
-        public string Name { get; set; }
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int StylistId { get; set; }
         public virtual Stylist Stylist { get; set; }
     }
