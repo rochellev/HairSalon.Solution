f140e167e0e1fe0ffddf488de28d0b4eba37c809
Student <example@example.com>
2019-08-20 08:47:56 -0700

    add initial migration

---
+++ HairSalon/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace HairSalon.Models
+{
+    public class SweetTreatContextFactory : IDesignTimeDbContextFactory<HairSalonContext>
+    {
+        HairSalonContext IDesignTimeDbContextFactory<HairSalonContext>.CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var builder = new DbContextOptionsBuilder<HairSalonContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseMySql(connectionString);
+
+            return new HairSalonContext(builder.Options);
+        }
+    }
+}
