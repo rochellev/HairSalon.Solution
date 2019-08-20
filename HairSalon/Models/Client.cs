519608e78fad7a0d49ba71ff69cc092a2a451ae4
rochellev <rochellevroberts@utexas.edu>
2019-08-20 08:20:15 -0700

    edit for entity

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
     public class Client
@@ -6,6 +8,11 @@ namespace HairSalon.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int StylistId { get; set; }
-        public virtual Stylist Stylist { get; set; }
+        public Stylist Stylist {get; set;}
+
+        public Client()
+        {
+            this.Stylist = new Stylist();
+        }
     }
 }
